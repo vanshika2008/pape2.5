29e5728d04775e05e1e047da0482cfeb4df75616
vanshika2008 <65905731+vanshika2008@users.noreply.github.com>
2020-10-01 23:27:08 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,7 +1,7 @@
 class Paper {
     constructor(x, y, width, height) {
       var options = {
-          isStatic:false,
+         // isStatic:false,
           restitution:0.3,
           friction:0.5,
           density:1.2
--- sketch.js
+++ sketch.js
@@ -6,14 +6,14 @@ var engine, world;
 
 var paper1,paper_image;
 var stick1,stick2,stick3;
-var ground,invisibleGround,ground2;
+var ground,invisibleGround,ground2,ground3,ground4;
 var dust,dust_image;
 function preload(){
   dust_image=loadImage("dustbingreen.png")
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(800, 500);
 	engine = Engine.create();
   world = engine.world;
   
@@ -22,20 +22,21 @@ function setup() {
 
   ground = new Ground(200,400,1000,10);
   invisibleGround = new Ground(200,390,1000,10);
-  invisibleGround.visible = false;
-
-  stick1 = new Stick(432, 341, 15,100);
-  stick1.visible = false;
-  stick2 = new Stick(548, 341, 15,100);
-  stick2.visible = false;
-  stick3 = new Stick(490, 382, 100,15);
-  stick3.visible = false;
   
-  dust=createSprite(490 , 341, 10,10);
+  stick1 = new Stick(447, 339, 15,102);
+  stick2 = new Stick(533, 339, 15,102);
+  stick3 = new Stick(490, 387, 100,15);
+  
+  dust=createSprite(490, 341, 10,10);
 	dust.addImage(dust_image);
-  dust.scale=0.4;
+  dust.scale=0.32;
 
-  ground2 = new Ground(490,377,100,50);   
+  ground2 = new Ground(490,377,100,15);  
+  invisibleground2 = new Ground(490,377,100,15);  
+  ground3 = new Ground(447, 341, 15,70);
+  innvisibleground3 = new Ground(447, 341, 15,70);
+  ground4 = new Ground(533, 341, 15,70);
+  invisibleground4 = new Ground(523, 341, 15,70);
 
 }
 
@@ -48,6 +49,8 @@ function draw() {
   keyPressed();
   ground.display();
   ground2.display();
+  ground3.display();
+  ground4.display();
   stick1.display();
   stick2.display();
   stick3.display();
@@ -58,20 +61,7 @@ function draw() {
 
 
 function keyPressed(){
-  if(keyCode === 32 && paper1.body.position.x < 320){
-     Matter.Body.applyForce(paper1.body,paper1.body.position,{x:5,y:-7});
+  if(keyCode === 38 && paper1.body.position.x < 320){
+     Matter.Body.applyForce(paper1.body,paper1.body.position,{x:0.2,y:-0.7});
   }
-  if(keyCode === 32 && paper1.body.position.x > 320){
-    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:0,y:18});
-  }
-
 }
-
-
-
-
-
-
-
-
-
